aa0bb09ab55252a1d7ec8880db41a68b6bc40b4b
Enrique Olivares <enrique.sandoval@outlook.de>
2023-12-19 16:05:44 -0500

    Landing page mejorada

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPortfolio(type) {
+    if (type === 'fotografo') {
+        window.location.href = 'https://www.behance.net/sayrolivares/projects';
+    } else if (type === 'analista') {
+        window.location.href = 'https://deepnote.com/@sayrolivares';
+    }
+}
